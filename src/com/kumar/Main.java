5b6c48d1ba959e82383587ed30098a446fd84222
Kumar,Nirmal <er.kumarnirmal@gmail.com>
2022-12-08 01:28:33 +0530

    variable classs over

--- src/com/kumar/Main.java
+++ src/com/kumar/Main.java
@@ -3,5 +3,14 @@ package com.kumar;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello Welcome to the java HCL trainings freshers");
+
+        // var declaration of primitive types
+        int age = 22;
+        System.out.println(age);
+
+        // var declaration of reference types
+
+        String name = new String("Java Training");
+        System.out.println(name.toUpperCase());
     }
 }
